1ca60eef91ad19a85d9c5a17ab1ffa6dc3a4b235
Andre Garcia <agarcia@octacorporate.com>
2022-11-14 22:16:38 +0000

    Responsive Fixes.

--- src/template/HomeDemo2/index.jsx
+++ src/template/HomeDemo2/index.jsx
@@ -54,7 +54,7 @@ let HomeDemo2 = () => {
         <SecDistribution img={HomeDemo2Allocation} data={TokenText} />
         <SecOurRoadmap data={timelineInfo} img={HomeDemo2BgRoadmap} />
         <SecFAQ_Timeline FQAInfo={FQAInfo} DocElementTitle={DocElementTitle} />
-        <SecTeam data={TeamMember} />
+        <SecTeam data={TeamMember} /> 
       </div>
     </>
   )
