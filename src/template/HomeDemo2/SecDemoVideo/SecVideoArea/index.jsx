7d7cf21809e0df96cf121ac2de6435b5712abbc8
Andre Garcia <agarcia@octacorporate.com>
2022-11-17 18:34:35 +0000

    MaisumUpdate

--- src/template/HomeDemo2/SecDemoVideo/SecVideoArea/index.jsx
+++ src/template/HomeDemo2/SecDemoVideo/SecVideoArea/index.jsx
@@ -1,19 +1,26 @@
-import YoutubeEmbed from '../../YoutubeEmbed'
+import ReactPlayer from 'react-player'
+
+const Embedvideo = (props) => {
+  return (
+    <div className="player-wrapper">
+      <ReactPlayer
+        className="react-player"
+        url={props.url}
+        width='100%'
+        height='100%'
+      />
+    </div>
+  )
+}
 
 const SecVideoArea = ({ img }) => {
+
   return (
     <div
-      className="col-lg-8 offset-lg-2 col-md-10 offset-md-1 col-sm-12"
+      className="col-lg-8 offset-lg-2 col-md-10 offset-md-1 col-sm-12 extra"
       data-aos="fade-up"
     >
-      <div className="welcome-video-area">
-        <div className="welcome-thumb">
-          <img src={img} alt="" />
-        </div>
-        <div className="video-icon">
-          <YoutubeEmbed embedId="yKlM2DZaG1I" />
-        </div>
-      </div>
+      <Embedvideo url={'https://youtu.be/yKlM2DZaG1I'} />
     </div>
   )
 }
