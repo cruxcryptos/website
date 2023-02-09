9e6f59c296223af565b9a87d857475ecf2b493d9
dev <info@cruxcryptos.com>
2023-02-09 01:11:50 +0000

    zombie cruxq

--- src/template/HomeDemo2/SecAboutUsClient/SecWhoWeContant/index.jsx
+++ src/template/HomeDemo2/SecAboutUsClient/SecWhoWeContant/index.jsx
@@ -9,7 +9,7 @@ const SecWhoWeContant = () => {
     <div className="col-12 col-lg-6" id='story'>
         <div className="who-we-contant zombie">
           <div className='zombie' data-aos="fade-up" >
-            <img src={image} akt="zombie crux"></img>
+            <img src={image} alt="zombie crux"></img>
           </div>
             <h4 data-aos="fade-up">Where it begins..</h4>
             <p data-aos="fade-up"> A lethal virus, spread all over the world through bites, has turned people into something toxic, unthinking and dangerous. As the pandemic threatens to consume humanity, the last humans try to survive and find a cure for the infection.</p>
